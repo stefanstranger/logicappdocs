f012a13ae70ae776f74ecd5b55132b8a063e8e50
Stefan Stranger <stefan.stranger@microsoft.com>
2023-06-23 14:55:40 +0200

    Added initial PSDocs template file

---
+++ src/LogicApp.Doc.ps1
@@ -0,0 +1,35 @@
+Document 'Azure-LogicApp-Documentation' {
+
+    "# Azure Logic App Documentation - $($InputObject.LogicApp.name)"
+
+    Section 'Introduction' {
+        "This document describes the Azure Logic App Workflow **$($InputObject.LogicApp.name)** in the **$($InputObject.LogicApp.ResourceGroupName)** resource group in the **$($InputObject.LogicApp.SubscriptionName)** subscription."
+        "This document is programmatically generated using a PowerShell script."
+
+        "Date: $(Get-Date -Format 'yyyy-MM-dd')"
+    }
+
+    Section 'Logic App Workflow Actions'{
+@"        
+```````mermaid
+$($InputObject.diagram)
+```````
+"@       
+}
+
+
+
+    Section 'Logic App Workflow Actions' {
+        "This section shows an overview of Logic App Workflow actions and their dependencies."
+
+        Section 'Actions' {
+
+            $($InputObject.actions) | 
+            ForEach-Object { $_ |
+                Table -Property 'ActionName', 'Type', 'RunAfter', 'Parent', 'ChildActions'
+                # Added empty line between each table
+                "<!--- added empty line -->"
+            }                
+        }
+    }
+}
