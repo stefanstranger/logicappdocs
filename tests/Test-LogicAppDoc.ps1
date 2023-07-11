06b7d2098f8024d8f024c6f0e8b883744544a2da
Stefan Stranger <stefan.stranger@microsoft.com>
2023-07-11 12:05:34 +0200

    Updated path

--- tests/Test-LogicAppDoc.ps1
+++ tests/Test-LogicAppDoc.ps1
@@ -8,7 +8,7 @@ $params = @{
     Location         = 'westeurope'
     FilePath         = '.\logic-jiraintegration-demo.json'
     LogicAppName     = 'logic-jiraintegration-demo'
-    OutputPath       = '..\examples\'
+    OutputPath       = '.\examples\'
     Verbose          = $false
     Debug            = $false
 }
