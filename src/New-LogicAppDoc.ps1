01e8035d974dc7c06265f7b73eca80e40254bdd8
Stefan Stranger <stefan.stranger@microsoft.com>
2023-08-05 15:35:59 +0200

    Updated version number. Changed ascii logo. Moved helper functions to helper script. Added logic when no connections are configured.

--- src/New-LogicAppDoc.ps1
+++ src/New-LogicAppDoc.ps1
@@ -32,13 +32,16 @@ $WarningPreference = 'SilentlyContinue'
 
 
 @"
-╭──────────────────────╮
-│    logicappdocs      │
-╰──────────────────────╯
-
+██╗      ██████╗  ██████╗ ██╗ ██████╗ █████╗ ██████╗ ██████╗ ██████╗  ██████╗  ██████╗███████╗
+██║     ██╔═══██╗██╔════╝ ██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔═══██╗██╔════╝██╔════╝
+██║     ██║   ██║██║  ███╗██║██║     ███████║██████╔╝██████╔╝██║  ██║██║   ██║██║     ███████╗
+██║     ██║   ██║██║   ██║██║██║     ██╔══██║██╔═══╝ ██╔═══╝ ██║  ██║██║   ██║██║     ╚════██║
+███████╗╚██████╔╝╚██████╔╝██║╚██████╗██║  ██║██║     ██║     ██████╔╝╚██████╔╝╚██████╗███████║
+╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═════╝  ╚═════╝  ╚═════╝╚══════╝
+                                                                                                                                                         
 Author: Stefan Stranger
 Github: https://github.com/stefanstranger/logicappdocs
-Version: 1.0.3
+Version: 1.1.0
 
 "@.foreach({
         Write-Host $_ -ForegroundColor Magenta
@@ -54,205 +57,16 @@ else {
 }
 #endregion
 
+#region dot source Helper Functions
+. (Join-Path $PSScriptRoot 'Helper.ps1')
+#endregion
+
 #region Set Variables
 $templateName = 'Azure-LogicApp-Documentation'
 $templatePath = (Join-Path $PSScriptRoot 'LogicApp.Doc.ps1')
 #endregion
 
 #region Helper Functions
-Function Get-Action {
-
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
-        $Actions,
-        [Parameter(Mandatory = $false)]
-        $Parent = $null
-    )
-
-    foreach ($key in $Actions.PSObject.Properties.Name) {
-        $action = $Actions.$key
-        $actionName = $key.Replace(' ', '_').Replace('(', '').Replace(')', '').Replace('|', '')
-
-        # new runafter code
-        $runAfter = if (![string]::IsNullOrWhitespace($action.runafter)) {
-            $action.runAfter.PSObject.Properties.Name.Replace(' ', '_').Replace('(', '').Replace(')', '').Replace('|', '')
-        }
-        elseif (([string]::IsNullOrWhitespace($action.runafter)) -and $Parent) {
-            # if Runafter is empty but has parent use parent.
-            $Parent -replace '(-False|-True)', ''
-        }
-        else {
-            # if Runafter is empty and has no parent use null.
-            $null
-        }
-        
-        $inputs = if ($action | Get-Member -MemberType Noteproperty -Name 'inputs') { 
-            $($action.inputs)
-        } 
-        else { 
-            $null 
-        }
-
-        $type = $action.type
-
-        # new ChildActions code
-        $childActions = if (($action | Get-Member -MemberType Noteproperty -Name 'Actions') -and ($action.Actions.PSObject.Properties | measure-object).count -gt 0) { $action.Actions.PSObject.Properties.Name } else { $null }
-        
-        # Create PSCustomObject
-        [PSCustomObject]@{
-            ActionName   = $actionName
-            RunAfter     = $runAfter
-            Type         = $type
-            Parent       = $Parent
-            ChildActions = $childActions
-            Inputs       = Remove-Secrets -Inputs $($inputs | ConvertTo-Json -Depth 10 -Compress)
-        }
-
-        if ($action.type -eq 'If') {
-            # Check if the else property is present
-            if ($action | Get-Member -MemberType Noteproperty -Name 'else') {
-                # Get the actions for the true condition
-                Write-Verbose -Message ('Processing action {0}' -f $actionName)
-                # Check if Action has any actions for the true condition
-                if (![string]::IsNullOrEmpty($action.actions)) { 
-                    Get-Action -Actions $($action.Actions) -Parent ('{0}-True' -f $actionName)
-                    # Get the actions for the false condition
-                    Get-Action -Actions $($action.else.Actions) -Parent ('{0}-False' -f $actionName)
-                }
-            }
-            #When there is only action for the true condition
-            else {
-                Get-Action -Actions $($action.Actions) -Parent ('{0}-True' -f $actionName)
-            }
-        }
-
-        # Recursively call the function for child actions
-        elseif ($action | Get-Member -MemberType Noteproperty -Name 'Actions') {
-            Get-Action -Actions $($action.Actions) -Parent $actionName
-        }
-    }   
-}
-
-Function Get-Connection {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
-        $Connection
-    )
-
-    foreach ($key in $Connection.PSObject.Properties) {
-        [PSCustomObject]@{
-            Name                 = $key.name
-            ConnectionId         = $key.Value.connectionId
-            ConnectionName       = $key.Value.connectionName
-            ConnectionProperties = if ($key.Value | Get-Member -MemberType NoteProperty connectionProperties) { $key.Value.connectionProperties } else { $null }
-            id                   = $key.Value.id
-        } 
-    }
-}
-Function Remove-Secrets {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
-        $Inputs
-    )
-
-    # Remove the secrets from the Logic App Inputs
-    $regexPattern = '(\"headers":\{"Authorization":"(Bearer|Basic) )[^"]*'
-    $Inputs -replace $regexPattern, '$1******'
-}
-
-Function Sort-Action {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
-        $Actions
-    )
-
-    # Search for the action that has an empty RunAfter property
-    $firstAction = $Actions | Where-Object { [string]::IsNullOrEmpty($_.RunAfter) } |
-    Add-Member -MemberType NoteProperty -Name Order -Value 0 -PassThru
-    $currentAction = $firstAction
-
-    # Define a variable to hold the current order index
-    $indexNumber = 1
-
-    #Loop through all the actions 
-    for ($i = 1; $i -lt $Actions.Count; $i++) {
-        Write-Verbose -Message ('Processing currentaction {0}' -f $($currentAction.ActionName))
-        # Search for the action that has the first action's ActionName in the RunAfter property or the previous action's ActionName
-        if (![string]::IsNullOrEmpty($firstAction)) {
-            $Actions | Where-Object { $_.RunAfter -eq $firstAction.ActionName } | 
-            Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber
-            $currentAction = ($Actions | Where-Object { $_.RunAfter -eq $firstAction.ActionName })
-            # Set the firstAction variable to null
-            $firstAction = $null            
-            $indexNumber++ 
-        }
-        else {
-            # Search for actions that have the previous action's ActionName in the RunAfter property
-            # If there are multiple actions with the same RunAfter property, set the RunAfter property to the Parent property
-            if (($Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) } | Measure-Object).count -gt 1) {
-                $Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) } | ForEach-Object {                     
-                    # Check if the action has a Parent Value
-                    if (![string]::IsNullOrEmpty($_.Parent)) {
-                        Write-Verbose -Message ('Setting RunAfter property {0} to Parent property value {1} for action {2}' -f $_.RunAfter, $_.Parent, $_.ActionName)
-                        $_.RunAfter = $_.Parent
-                    }
-                }
-                # Iterate first the condition status true actions.
-                if ($Actions | Where-Object { $_.RunAfter -eq $(('{0}-True') -f $($currentAction.ActionName)) }) {
-                    $Actions | Where-Object { $_.RunAfter -eq $(('{0}-True') -f $($currentAction.ActionName)) }  |
-                    Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber 
-                    $currentAction = $Actions | Where-Object { $_.RunAfter -eq $(('{0}-True') -f $($currentAction.ActionName)) } 
-                    # Increment the indexNumber
-                    $indexNumber++
-                }   
-                else {
-                    # Add Order property to the action that it's RunAfter property updated from the Parent property.
-                    # This is the first action in a foreach loop.
-                    # After this action the rest of rest of the foreach actions need to be processed.
-                    $Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) -and ($null -ne $($_.Parent)) } |
-                    Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber 
-                    $currentAction = $Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) -and ($null -ne $($_.Parent)) }
-                    # Increment the indexNumber
-                    $indexNumber++
-                }             
-            }
-            else {
-                # If there cannot any action found with the previous action's ActionName in the RunAfter property, search for the action has a parent with the false condition.
-                if ($Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) }) {
-                    $Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) } | 
-                    Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber 
-                    # CurrentAction will be empty if the ??
-                    $currentAction = ($Actions | Where-Object { $_.RunAfter -eq $($currentAction.ActionName) })
-                    # Increment the indexNumber
-                    $indexNumber++                    
-                }
-                elseif ($Actions | Where-Object { $_.RunAfter -eq $(('{0}-False') -f $(($currentAction.Parent).Substring(0, ($currentAction.Parent).length - 5))) } ) {
-                    $Actions | Where-Object { $_.RunAfter -eq $(('{0}-False') -f $(($currentAction.Parent).Substring(0, ($currentAction.Parent).length - 5))) }  |
-                    Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber 
-                    $currentAction = $Actions | Where-Object { $_.RunAfter -eq $(('{0}-False') -f $(($currentAction.Parent).Substring(0, ($currentAction.Parent).length - 5))) }
-                    # Increment the indexNumber
-                    $indexNumber++
-                }
-                else {
-                    # If there cannot any action found with the previous action's ActionName in the RunAfter property, search for the action has a parent with the same name as the currents action's runasfter property.
-                    if ($Actions | Where-Object { $_.RunAfter -eq $($currentAction.Parent) -and !($_ | Get-Member -MemberType NoteProperty 'Order') }) {
-                        $Actions | Where-Object { $_.RunAfter -eq $($currentAction.Parent) -and !($_ | Get-Member -MemberType NoteProperty 'Order') } | 
-                        Add-Member -MemberType NoteProperty -Name Order -Value $indexNumber 
-                        # CurrentAction will be empty if the ??
-                        $currentAction = ($Actions | Where-Object { ($_ | Get-Member -MemberType NoteProperty 'Order') -and ($_.Order -eq $indexNumber) })
-                        # Increment the indexNumber
-                        $indexNumber++                    
-                    }
-                }                         
-            }                
-        }
-        Write-Verbose -Message ('Current action {0} with Order Id {1}' -f $($currentAction.ActionName), $($currentAction.Order) )
-    }
-}
 
 # From PowerShell module AzViz. (https://raw.githubusercontent.com/PrateekKumarSingh/AzViz/master/AzViz/src/private/Test-AzLogin.ps1)
 Function Test-AzLogin {
@@ -321,7 +135,12 @@ if (!($FilePath)) {
     $Objects = Get-Action -Actions $($LogicApp.properties.definition.actions)
 
     # Get Logic App Connections
-    $Connections = Get-Connection -Connection $($LogicApp.properties.parameters.'$connections'.value)
+    if ($LogicApp.properties.parameters | Get-Member -MemberType NoteProperty -Name '$connections') {
+        $Connections = Get-Connection -Connection $($LogicApp.properties.parameters.'$connections'.value)
+    }
+    else {
+        $Connections = $null
+    }
 }
 else {
     Write-Output -InputObject ('Using Logic App Workflow code from file "{0}"' -f $FilePath)
@@ -330,9 +149,13 @@ else {
     $Objects = Get-Action -Actions $($LogicApp.definition.actions)
 
     # Get Logic App Connections
-    # For PowerApps Flows the connections are stored in a connectionMap json file. Skipp for now.
     if ($LogicApp | Get-Member -MemberType NoteProperty -Name 'parameters') {
-        $Connections = Get-Connection -Connection $($LogicApp.parameters.'$connections'.value)
+        if ($LogicApp.parameters | Get-Member -MemberType NoteProperty -Name '$connections') {
+            $Connections = Get-Connection -Connection $($LogicApp.parameters.'$connections'.value)
+        }
+        else {
+            $Connections = $null
+        }
     }
     else {
         $Connections = $null
