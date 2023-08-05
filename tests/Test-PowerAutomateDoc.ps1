64940567e882de3380cd2fbe62e549dbd64b182e
Stefan Stranger <stefan.stranger@microsoft.com>
2023-08-05 17:25:13 +0200

    Updated test script

--- tests/Test-PowerAutomateDoc.ps1
+++ tests/Test-PowerAutomateDoc.ps1
@@ -1,10 +1,10 @@
 <#
-    Script to test the PowerShell script New-LogicAppDoc.ps1 using a json file with the Logic App Workflow configuration
+    Script to test the PowerShell script New-PowerAutomateDoc.ps1 using a json file with the Logic App Workflow configuration
 #>
 
 $params = @{
     EnvironmentName = '839eace6-59ab-4243-97ec-a5b8fcc104e4'
-    PowerAutomateName     = 'Teams Chat birthday message bot flow - BirthDay Test Chat Group - SharePoint List'
+    PowerAutomateName     = 'Notify of Canceled Meetings'
     OutputPath       = '..\examples\'
     Verbose          = $false
     Debug            = $false
